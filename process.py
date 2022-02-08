0a3da411e77b84ee712ffe977da98ba0b4a0ad50
Dehar369 <80791539+Dehar369@users.noreply.github.com>
2022-02-08 07:08:23 +0100

    Add files via upload

---
+++ process.py
@@ -0,0 +1,133 @@
+from typing import Dict
+
+import SimpleITK
+import tqdm
+import json
+from pathlib import Path
+import tifffile
+import numpy as np
+from keras.layers import Flatten,Dense
+from keras.models import Model
+import cv2
+import tensorflow as tf
+
+
+
+from evalutils import ClassificationAlgorithm
+from evalutils.validators import (
+    UniquePathIndicesValidator,
+    UniqueImagesValidator,
+)
+from evalutils.io import ImageLoader
+
+
+class DummyLoader(ImageLoader):
+    @staticmethod
+    def load_image(fname):
+        return str(fname)
+
+
+    @staticmethod
+    def hash_image(image):
+        return hash(image)
+
+
+class airogs_algorithm(ClassificationAlgorithm):
+    def __init__(self):
+        super().__init__(
+            validators=dict(
+                input_image=(
+                    UniqueImagesValidator(),
+                    UniquePathIndicesValidator(),
+                )
+            ),
+        )
+
+        self._file_loaders = dict(input_image=DummyLoader())
+
+        self.output_keys = ["multiple-referable-glaucoma-likelihoods", 
+                            "multiple-referable-glaucoma-binary",
+                            "multiple-ungradability-scores",
+                            "multiple-ungradability-binary"]
+    
+    def load(self):
+        for key, file_loader in self._file_loaders.items():
+            fltr = (
+                self._file_filters[key] if key in self._file_filters else None
+            )
+            self._cases[key] = self._load_cases(
+                folder=Path("/input/images/color-fundus/"),
+                file_loader=file_loader,
+                file_filter=fltr,
+            )
+
+        pass
+    
+    def combine_dicts(self, dicts):
+        out = {}
+        for d in dicts:
+            for k, v in d.items():
+                if k not in out:
+                    out[k] = []
+                out[k].append(v)
+        return out
+    
+    def process_case(self, *, idx, case):
+        # Load and test the image(s) for this case
+        if case.path.suffix == '.tiff':
+            results = []
+            with tifffile.TiffFile(case.path) as stack:
+                for page in tqdm.tqdm(stack.pages):
+                    input_image_array = page.asarray()
+                    results.append(self.predict(input_image_array=input_image_array))
+        else:
+            input_image = SimpleITK.ReadImage(str(case.path))
+            input_image_array = SimpleITK.GetArrayFromImage(input_image)
+            results = [self.predict(input_image_array=input_image_array)]
+        
+        results = self.combine_dicts(results)
+
+        # Test classification output
+        if not isinstance(results, dict):
+            raise ValueError("Expected a dictionary as output")
+
+        return results
+
+    def predict(self, *,input_image_array : np.ndarray) -> Dict:
+
+
+        input_image_arrayy = cv2.resize(input_image_array,(512,512))
+        image = np.expand_dims(input_image_arrayy,axis=0)
+        image = image/255.
+
+        model = tf.keras.models.load_model('./resnet101.h5')
+
+        rg_likelihood = model.predict(image)
+        rg_binary = bool(rg_likelihood > .60)
+        if rg_likelihood >=0.4 and rg_likelihood<=0.6:
+            ungradability_binary = True
+            ungradability_score = 1.0
+        else:
+            ungradability_binary = False
+            ungradability_score = 0.0
+
+        out = {
+            "multiple-referable-glaucoma-likelihoods": rg_likelihood,
+            "multiple-referable-glaucoma-binary": rg_binary,
+            "multiple-ungradability-scores": ungradability_score,
+            "multiple-ungradability-binary": ungradability_binary
+        }
+
+        return out
+
+    def save(self):
+        for key in self.output_keys:
+            with open(f"/output/{key}.json", "w") as f:
+                out = []
+                for case_result in self._case_results:
+                    out += case_result[key]
+                json.dump(out, f)
+
+
+if __name__ == "__main__":
+    airogs_algorithm().process()
